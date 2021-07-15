c942d5bf5e9cb006b9f803656189bd7c9b0ddf92
kseniia-tester <82049513+kseniia-tester@users.noreply.github.com>
2021-07-15 12:57:32 +0300

    Update CashbackHackServiceTest.java

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,47 +1,33 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
+    private  CashbackHackService service = new CashbackHackService();
     @Test
     public void checkFor1Ruble() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1;
 
-        int actual = service.remain(amount);
-        int expected = 999;
-        assertEquals(expected, actual);
+
+        assertEquals(CashbackHackService.remain(1), 999);
     }
 
     @Test
     public void checkFor999Ruble() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 999;
 
-        int actual = service.remain(amount);
-        int expected = 1;
-        assertEquals(expected, actual);
+        assertEquals(CashbackHackService. remain(999), 1);
     }
 
     @Test
     public void checkFor1000Ruble() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1000;
 
-        int actual = service.remain(amount);
-        int expected = 0;
-        assertEquals(expected, actual);
+        assertEquals(CashbackHackService.remain(1000), 0);
     }
 
     @Test
     public void checkFor1100Ruble() {
-        CashbackHackService service = new CashbackHackService();
-        int amount = 1100;
 
-        int actual = service.remain(amount);
-        int expected = 900;
-        assertEquals(expected, actual);
+        assertEquals(CashbackHackService.remain(1100), 900);
     }
 }
